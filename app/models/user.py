041bb7a52e7a205cc41e0b36c3bdc3c2067b3410
Petrov Egor <petrovegor495@gmail.com>
2025-08-28 19:47:17 +0400

    feat: настроена конфигурация, БД и Alembic, создана модель User

---
+++ app/models/user.py
@@ -0,0 +1,19 @@
+import enum
+from sqlalchemy import Column, Integer, String, Enum
+from app.core.database import Base
+
+
+class Role(str, enum.Enum):
+    CANDIDATE = "CANDIDATE"
+    HR = "HR"
+
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    email = Column(String, unique=True, index=True, nullable=False)
+    name = Column(String, nullable=False)
+    company_name = Column(String, nullable=True)  # Только для HR
+    hashed_password = Column(String, nullable=False)
+    role = Column(Enum(Role), nullable=False)
